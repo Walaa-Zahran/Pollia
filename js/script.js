b59371ada1b3a269ddffe8b8adb62760d7271367
Walaa Zahran <walaazahran183@gmail.com>
2018-08-27 23:19:25 +0200

    last edition by walaa zahran

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
+function show1(){
+	window.location.replace("../docs/index-contact.html");
+}
+
+function show2(){
+	window.location.replace("../docs/index-4.html");
+}
+function show3(){
+	window.location.replace("../docs/index-5.html");
+}
+function show4(){
+	window.location.replace("../docs/index-6.html");
+}
+function show5(){
+	window.location.replace("../docs/index-7.html");
+}
+function show6(){
+	window.location.replace("../docs/index-8.html");
+}
+function show7(){
+	window.location.replace("../docs/index-9.html");
+}
+function show8(){
+	window.location.replace("../docs/index-10.html");
+}
+function show9(){
+	window.location.replace("../docs/index-11.html");
+}
+function show10(){
+	window.location.replace("../docs/index-12.html");
+	
+}
